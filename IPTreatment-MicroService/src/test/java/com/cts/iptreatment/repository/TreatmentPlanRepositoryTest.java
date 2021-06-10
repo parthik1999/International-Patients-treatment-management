5c9ad878e946f5f1c4d763f71304ea79025433b0
Parthik Shah <parthik1999@gmail.com>
2021-06-10 16:05:59 +0530

    IPTratment Microservices

---
+++ IPTreatment-MicroService/src/test/java/com/cts/iptreatment/repository/TreatmentPlanRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.cts.iptreatment.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.cts.iptreatment.model.TreatmentPlan;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class TreatmentPlanRepositoryTest {
+	
+	@Autowired
+	private TreatmentPlanRepository treatmentPlanRepo;
+
+	@Test
+	public void testSaveTreatmentPlan() {
+		TreatmentPlan plan = new TreatmentPlan();
+		assertThat(treatmentPlanRepo.save(plan).equals(plan));
+	}
+	
+	@Test
+	public void testGetTreatmentPlan() {
+		assertThat(treatmentPlanRepo.findAll().isEmpty());
+	}
+}
